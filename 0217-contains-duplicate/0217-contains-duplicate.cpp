4b623f6125aadd49b5d48769e0c0db21bcd7a91e
Harshit Sanwal <harshitsanwal2007@gmail.com>
2026-07-17 15:29:10 +0530

    Time: 28 ms (83.05%), Space: 82.7 MB (81.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,7 +3,6 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        int count=0;
         for(int i=0;i<n-1;i++){
             if(nums[i]==nums[i+1])
              return 1;
